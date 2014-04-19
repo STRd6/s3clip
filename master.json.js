a0a7f093e5828dbc71dd38159c49b9043bd57f32
Daniel X Moore <yahivin@gmail.com>
2014-04-18 20:55:04 -0700

    :clock5::rabbit2: Updated in browser at strd6.github.io/editor

--- master.json.js
+++ master.json.js
@@ -14,7 +14,12 @@ window["STRd6/s3clip:master"]({
     },
     "main.coffee.md": {
       "path": "main.coffee.md",
-      "content": "S3 Clip\n=======\n\nClip images to an S3 bucket.\n\n    chrome.contextMenus.create \n        title: \"Send to S3\"\n        contexts:[\"image\"]\n        onclick: (info) ->\n          console.log info\n          alert \"yolo\"\n",
+      "content": "S3 Clip\n=======\n\nClip images to an S3 bucket.\n\n    chrome.contextMenus.create \n      title: \"Send to S3\"\n      contexts:[\"image\"]\n      onclick: (info) ->\n        console.log info\n        alert \"yolo\"\n",
+      "mode": "100644"
+    },
+    "pixie.cson": {
+      "path": "pixie.cson",
+      "content": "author: \"STRd6\"\nname: \"S3 Clip\"\ndescription: \"Clip images to S3\"\nversion: \"0.1.0\"\nwidth: 480\nheight: 320\nentryPoint: \"main\"\npermissions: [\n  \"<all_urls>\"\n]\n",
       "mode": "100644"
     }
   },
@@ -23,11 +28,17 @@ window["STRd6/s3clip:master"]({
       "path": "main",
       "content": "(function() {\n  chrome.contextMenus.create({\n    title: \"Send to S3\",\n    contexts: [\"image\"],\n    onclick: function(info) {\n      console.log(info);\n      return alert(\"yolo\");\n    }\n  });\n\n}).call(this);\n",
       "type": "blob"
+    },
+    "pixie": {
+      "path": "pixie",
+      "content": "module.exports = {\"author\":\"STRd6\",\"name\":\"S3 Clip\",\"description\":\"Clip images to S3\",\"version\":\"0.1.0\",\"width\":480,\"height\":320,\"entryPoint\":\"main\",\"permissions\":[\"<all_urls>\"]};",
+      "type": "blob"
     }
   },
   "progenitor": {
     "url": "http://www.danielx.net/editor/"
   },
+  "version": "0.1.0",
   "entryPoint": "main",
   "repository": {
     "branch": "master",
