a408c77bf7a70109b62f4045c831511c6b4e2924
Daniel X Moore <yahivin@gmail.com>
2014-04-18 20:53:14 -0700

    :baby::mailbox_with_mail: Updated in browser at strd6.github.io/editor

---
+++ master.json.js
@@ -0,0 +1,43 @@
+window["STRd6/s3clip:master"]({
+  "source": {
+    "LICENSE": {
+      "path": "LICENSE",
+      "content": "The MIT License (MIT)\n\nCopyright (c) 2014 Daniel X Moore\n\nPermission is hereby granted, free of charge, to any person obtaining a copy\nof this software and associated documentation files (the \"Software\"), to deal\nin the Software without restriction, including without limitation the rights\nto use, copy, modify, merge, publish, distribute, sublicense, and/or sell\ncopies of the Software, and to permit persons to whom the Software is\nfurnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all\ncopies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\nIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\nFITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\nAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\nLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\nOUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\nSOFTWARE.",
+      "mode": "100644",
+      "type": "blob"
+    },
+    "README.md": {
+      "path": "README.md",
+      "content": "s3clip\n======\n\nCopy images to S3\n",
+      "mode": "100644",
+      "type": "blob"
+    },
+    "main.coffee.md": {
+      "path": "main.coffee.md",
+      "content": "S3 Clip\n=======\n\nClip images to an S3 bucket.\n\n    chrome.contextMenus.create \n        title: \"Send to S3\"\n        contexts:[\"image\"]\n        onclick: (info) ->\n          console.log info\n          alert \"yolo\"\n",
+      "mode": "100644"
+    }
+  },
+  "distribution": {
+    "main": {
+      "path": "main",
+      "content": "(function() {\n  chrome.contextMenus.create({\n    title: \"Send to S3\",\n    contexts: [\"image\"],\n    onclick: function(info) {\n      console.log(info);\n      return alert(\"yolo\");\n    }\n  });\n\n}).call(this);\n",
+      "type": "blob"
+    }
+  },
+  "progenitor": {
+    "url": "http://www.danielx.net/editor/"
+  },
+  "entryPoint": "main",
+  "repository": {
+    "branch": "master",
+    "default_branch": "master",
+    "full_name": "STRd6/s3clip",
+    "homepage": null,
+    "description": "Copy images to S3",
+    "html_url": "https://github.com/STRd6/s3clip",
+    "url": "https://api.github.com/repos/STRd6/s3clip",
+    "publishBranch": "gh-pages"
+  },
+  "dependencies": {}
+});
